e93c3f522d126af50fd531a318ab1f97c048bada
cjm0225 <572007142@qq.com>
2024-07-14 00:11:44 +0800

    add my avatar png

--- src/vue/navigation/default/NavSidebar.vue
+++ src/vue/navigation/default/NavSidebar.vue
@@ -3,14 +3,14 @@
         <!-- Main Content -->
         <div class="nav-sidebar-content" v-if="profileData">
             <!-- Profile Card -->
-            <NavProfileCard :profile-data="profileData" :shrink="!navigation.isSidebarExpanded()"/>
+            <NavProfileCard :profile-data="profileData" :shrink="!navigation.isSidebarExpanded()" />
 
             <!-- Nav Link List -->
             <ul class="nav-links">
                 <!-- Nav Link -->
                 <li v-for="section in data.getSections()" :class="_getNavItemClassList(section)">
                     <button class="nav-link" @click="_onLinkClicked(section)">
-                        <i :class="section['faIcon']"/>
+                        <i :class="section['faIcon']" />
                         <span>{{ data.getString(section['id']) }}</span>
                     </button>
                 </li>
@@ -20,16 +20,16 @@
         <!-- Footer -->
         <div class="nav-sidebar-footer" v-if="profileData">
             <!-- Language Picker -->
-            <LanguagePicker :display-language-label="navigation.isSidebarExpanded()" class="language-picker"/>
+            <LanguagePicker :display-language-label="navigation.isSidebarExpanded()" class="language-picker" />
 
             <!-- Credits -->
             <div class="nav-sidebar-footer-credits text-2 mt-3" :class="!canShrink ? 'mb-3' : 'mb-0'">
-                <span v-html="profileData['locales']['credits']"/>
+                <span v-html="profileData['locales']['credits']" />
             </div>
 
             <!-- Toggle Button -->
             <button class="nav-toggle-button mt-3" @click="_onToggleButton()" v-if="canShrink">
-                <i class="fa-solid" :class="navigation.isSidebarExpanded() ? 'fa-caret-left' : 'fa-caret-right'"/>
+                <i class="fa-solid" :class="navigation.isSidebarExpanded() ? 'fa-caret-left' : 'fa-caret-right'" />
             </button>
         </div>
     </nav>
@@ -38,16 +38,16 @@
 <script setup>
 import LanguagePicker from "../../widgets/LanguagePicker.vue"
 import NavProfileCard from "../partials/NavProfileCard.vue"
-import {computed, onMounted} from "vue"
-import {useData} from "../../../composables/data.js"
-import {useNavigation} from "../../../composables/navigation.js"
+import { computed, onMounted } from "vue"
+import { useData } from "../../../composables/data.js"
+import { useNavigation } from "../../../composables/navigation.js"
 
 const emit = defineEmits(['linkClicked'])
 const data = useData()
 const navigation = useNavigation()
 
 onMounted(() => {
-    if(!canShrink.value) {
+    if (!canShrink.value) {
         navigation.setSidebarStatus(true)
     }
 })
@@ -69,7 +69,7 @@ const profileData = computed(() => {
  */
 const _getNavItemClassList = (section) => {
     let classList = 'nav-item'
-    if(navigation.isSectionActive(section['id'])) {
+    if (navigation.isSectionActive(section['id'])) {
         classList += ' nav-item-selected'
     }
 
@@ -119,7 +119,7 @@ ul.nav-links {
     list-style: none;
 
     @media screen and (min-height: 780px) {
-        padding-top:1rem;
+        padding-top: 1rem;
     }
 }
 
@@ -150,6 +150,7 @@ li.nav-item {
 
         &:hover {
             color: $nav-item-lighten-color;
+
             i {
                 color: $nav-item-lighten-strong-color;
             }
@@ -159,6 +160,7 @@ li.nav-item {
     &.nav-item-selected {
         .nav-link {
             color: $nav-item-lighten-color;
+
             i {
                 color: $nav-item-lighten-strong-color;
             }
@@ -169,13 +171,14 @@ li.nav-item {
 .nav-sidebar-footer {
     text-align: center;
     padding: 1rem;
-    color:$white;
+    color: $white;
 
     .nav-sidebar-footer-credits {
         font-family: $custom-subheadings-font-family;
         color: $light-4;
 
         display: block;
+
         @media screen and (max-height: 680px) {
             display: none;
         }
@@ -187,7 +190,7 @@ li.nav-item {
     align-items: center;
     justify-content: center;
 
-    border:none;
+    border: none;
     border-radius: 100%;
 
     width: 32px;
@@ -217,6 +220,7 @@ li.nav-item {
         display: flex;
         align-items: center;
         justify-content: center;
+
         span {
             display: none;
         }
@@ -231,12 +235,12 @@ li.nav-item {
     }
 
     .nav-toggle-button {
-        margin: 1.5rem 0 1rem!important;
+        margin: 1.5rem 0 1rem !important;
     }
 
     @media screen and (max-height: 620px) {
         ul.nav-links {
-            margin-bottom: 0.5rem!important;
+            margin-bottom: 0.5rem !important;
         }
 
         .language-picker {
@@ -248,7 +252,7 @@ li.nav-item {
         }
 
         .nav-toggle-button {
-            margin-top: 0!important;
+            margin-top: 0 !important;
         }
     }
 
